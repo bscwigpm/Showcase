113cc8962d63e17e87163a79578d18ba9ef4524a
Andreas Martin <andreas.martin@andreasmartin.ch>
2014-04-19 19:04:20 +0200

    LoanApprovalBPMNESB start

---
+++ LoanApprovalBPMNESB/src/main/java/ch/fhnw/bscwi/gpm/loanapproval/service/CustomerService.java
@@ -0,0 +1,30 @@
+package ch.fhnw.bscwi.gpm.loanapproval.service;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.camunda.bpm.engine.cdi.BusinessProcess;
+import org.camunda.bpm.engine.cdi.annotation.ProcessVariable;
+
+@Named
+public class CustomerService {
+
+	@Inject
+	@ProcessVariable
+	public Object customerId;
+
+	@Inject
+	private BusinessProcess businessProcess;
+
+	public void loadCustomer() {
+		/*LoanApprovalESBService_Service service = new LoanApprovalESBService_Service();
+        LoanApprovalESBService port = service.getLoanApprovalESBServiceSOAP();
+        CustomerDTO customerDTO = port.loadCustomer(Long
+				.valueOf((String) customerId));
+		if (customerDTO != null) {
+			businessProcess.setVariable("customerFirstName", customerDTO.getFirstName());
+			businessProcess.setVariable("customerLastName", customerDTO.getLastName());
+		}*/
+	}
+
+}
